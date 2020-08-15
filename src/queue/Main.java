1f6685be66084a9c731d0992b0b3ced527ff6465
yuibo <hcy_0622@163.com>
2020-08-15 18:32:41 +0800

    添加队列数据结构，包含数组队列和循环队列

---
+++ src/queue/Main.java
@@ -0,0 +1,67 @@
+package queue;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        Queue<Integer> queue = new ArrayQueue<>();
+//        for (int i = 0; i < 10; i++) {
+//            queue.enqueue(i);
+//            System.out.println(queue);
+//
+//            if (i % 3 == 2) {
+//                queue.dequeue();
+//                System.out.println(queue);
+//            }
+//        }
+
+//        Queue<Integer> queue = new LoopQueue<>();
+//        for (int i = 0; i < 10; i++) {
+//            queue.enqueue(i);
+//            System.out.println(queue);
+//
+//            if (i % 3 == 2) {
+//                queue.dequeue();
+//                System.out.println(queue);
+//            }
+//        }
+
+//        Queue<Integer> queue = new LinkedListQueue<>();
+//        for (int i = 0; i < 10; i++) {
+//            queue.enqueue(i);
+//            System.out.println(queue);
+//
+//            if (i % 3 == 2) {
+//                queue.dequeue();
+//                System.out.println(queue);
+//            }
+//        }
+
+        int opCount = 100000;
+
+        Queue<Integer> arrayQueue = new ArrayQueue<>();
+        double time1 = testQueue(arrayQueue, opCount);
+        System.out.println("ArrayQueue, time: " + time1 + " s");
+
+        Queue<Integer> loopQueue = new LoopQueue<>();
+        double time2 = testQueue(loopQueue, opCount);
+        System.out.println("LoopQueue, time: " + time2 + " s");
+
+        Queue<Integer> linkedListQueue = new LinkedListQueue<>();
+        double time3 = testQueue(linkedListQueue, opCount);
+        System.out.println("LinkedListQueue, time: " + time3 + " s");
+    }
+
+    private static double testQueue(Queue<Integer> q, int opCount) {
+        long startTime = System.nanoTime();
+
+        Random random = new Random();
+        for (int i = 0; i < opCount; i++) q.enqueue(random.nextInt(Integer.MAX_VALUE));
+        for (int i = 0; i < opCount; i++) q.dequeue();
+
+        long endTime = System.nanoTime();
+
+        return (endTime - startTime) / 1000000000.0;
+    }
+}
